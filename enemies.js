69ca661cde77b4e726aa91dc3b3c014d7340c45d
Andrii Doroshenko <andry.dorosh@gmail.com>
2020-08-22 14:27:31 +0300

    Create enemies.js

---
+++ enemies.js
@@ -0,0 +1,22 @@
+export const enemies = [
+  {
+    look: `💩`,
+    value: 3,
+    advancedEffect: `sub-creation`
+  },
+  {
+    look: `😈`,
+    value: 5,
+    advancedEffect: `round-move`
+  },
+  {
+    look: `🤡`,
+    value: 7,
+    advancedEffect: `dodge`
+  },
+  {
+    look: `👻`,
+    value: 9,
+    advancedEffect: `speed-move`
+  },
+];
