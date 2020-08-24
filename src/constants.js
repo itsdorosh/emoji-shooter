7d4b5227a60776d02c682cd60793dd2255418ae2
Andrii Doroshenko <andry.dorosh@gmail.com>
2020-08-24 22:03:15 +0300

    add basic controls contract
    update implementation of init method
    add comments

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,5 @@ const GAME_EVENT_MAP = {
 	'shot': ['click', 'gamepad-click'],
 	'aim': ['mousemove', 'gamepad-move'],
 };
+
+const AVAILABLE_DOM_EVENTS = ['click', 'mousemove'];
--- src/controls.js
+++ src/controls.js
@@ -1,3 +1,8 @@
+/**
+ * @class Controls
+ * @description this class provide API for interaction with several controls for example mouse or gamepad
+ * you can simply subscribe
+ */
 class Controls {
 	rootObject;
 
@@ -8,24 +13,30 @@ class Controls {
 
 	/**
 	 * @method setRootObjectForSubscriptions
-	 * this method allows you set container,
-	 * from witch we should be notified about user interactions
+	 * @description this method allows you set container, from witch we should be notified about user interactions
 	 * @param rootObject - an HTMLElement for subscriptions
 	 */
 	setRootObjectForSubscriptions(rootObject = document.body) {
 		this.rootObject = rootObject;
 	}
 
+	/**
+	 * @method init
+	 */
 	init() {
-		for (let gameEvent in GAME_EVENT_MAP) {
-			GAME_EVENT_MAP[gameEvent].forEach((mappedDOMEvent) => {
-				this.rootObject.addEventListener(mappedDOMEvent, (DOMEvent) => {
-					this.dispatch(gameEvent, DOMEvent);
-				});
+		AVAILABLE_DOM_EVENTS.forEach((eventName) => {
+			this.rootObject.addEventListener(eventName, (event) => {
+				this.dispatch(eventName, event);
 			});
-		}
+		});
 	}
 
+	/**
+	 * @method on
+	 * @description this method should assign some callback to eventName and prevent secondary assignment
+	 * @param eventName
+	 * @param callback
+	 */
 	on(eventName, callback) {
 		if (!eventName in this.eventStorage) {
 			this.eventStorage[eventName] = callback;
@@ -34,12 +45,25 @@ class Controls {
 		}
 	}
 
+	/**
+	 * @method dispatch
+	 * @description this method should provide way for trigger some callback by eventName
+	 * @param eventName
+	 * @param DOMEvent
+	 */
 	dispatch(eventName, DOMEvent) {
 		if (eventName in this.eventStorage) {
 			this.eventStorage[eventName](DOMEvent);
+		} else {
+			throw(`there is no callback for ${eventName} event name`);
 		}
 	}
 
+	/**
+	 * @method unsubscribe
+	 * @description this method allows you remove subscription assigned to the eventName
+	 * @param eventName
+	 */
 	unsubscribe(eventName) {
 		if (eventName in this.eventStorage) {
 			delete this.eventStorage[eventName];
